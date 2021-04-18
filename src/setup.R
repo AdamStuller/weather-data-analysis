112671e0fec619bf383a645aed2edc8467d4379b
Denisa Mensatorisová <32281312+DeMensa123@users.noreply.github.com>
2021-04-18 14:54:16 +0200

    Edit slp, wnd, vis

--- src/setup.R
+++ src/setup.R
@@ -4,3 +4,10 @@ library(Hmisc)
 library(data.table)
 library(funModeling)
 library(corrplot)
+
+
+# mode function
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
